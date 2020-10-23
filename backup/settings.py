6c4f0264fb7e0812dc340b5d76c819103bf463fa
Kate <you@example.com>
2020-10-23 17:33:52 +0300

    profile added

--- backup/settings.py
+++ backup/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudbackup.apps.CloudbackupConfig',
+    'users.apps.UsersConfig',
+    'bootstrap3',
+    'registration',
 
 
 ]
